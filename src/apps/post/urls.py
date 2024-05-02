7533efc0a3ef5c8f06e119542ab03bf202901275
Rafael Maia <rafaeucaetanoquintas@gmail.com>
2024-05-02 11:57:08 -0300

    added post update delete api view to urls

--- src/apps/post/urls.py
+++ src/apps/post/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'post'
 
 urlpatterns = [
-    path('posts', views.PostListCreate.as_view(), name='post-view-create'),
+    path('posts/', views.PostListCreate.as_view(), name='post-create'),
+    path('posts/<int:pk>/', views.PostRetrieveUpdateDestroy.as_view(), name='post-retrieve-update-destroy'),
 ]
