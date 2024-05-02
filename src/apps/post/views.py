bbb9a426f72c23bcdbfa994f52586f210d374c8b
Rafael Maia <rafaeucaetanoquintas@gmail.com>
2024-05-02 12:00:04 -0300

    customized list create api view so i can delete all posts at once

--- src/apps/post/views.py
+++ src/apps/post/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from .models import Post
 from .serializers import PostSerializer
@@ -7,6 +8,10 @@ from .serializers import PostSerializer
 class PostListCreate(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def delete(self, request, *args, **kwargs):
+        Post.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class PostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
