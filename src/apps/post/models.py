f617c8a123c3bddebb71740ad0436f2b992765d8
Rafael Maia <rafaeucaetanoquintas@gmail.com>
2024-05-02 11:17:55 -0300

    created Post model

--- src/apps/post/models.py
+++ src/apps/post/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts', verbose_name='Author')
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique_for_date='publish_date')
+    body = models.TextField()
+    published_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
