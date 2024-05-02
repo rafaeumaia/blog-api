3e92e7ca12452b5ed2e80dad6be9a5fb0301369e
Rafael Maia <rafaeucaetanoquintas@gmail.com>
2024-05-02 11:43:21 -0300

    post create api view

--- src/apps/post/views.py
+++ src/apps/post/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostListCreate(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
